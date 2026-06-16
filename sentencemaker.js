e38d0025989c3c2da14e6cbf3ee3b51313512995
Salvatorepolise <salvatorepolisee@gmail.com>
2026-06-16 14:50:55 +0200

    Today I learned this

---
+++ sentencemaker.js
@@ -0,0 +1,24 @@
+let adjective;
+let noun;
+let verb;
+let place;
+let adjective2;
+let noun2;
+
+adjective = "tiny";
+noun = "man";
+verb = "needed";
+place = "donutshop";
+adjective2 = "big";
+noun2 = "donuts";
+const firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+
+adjective = "beautiful";
+noun = "girl";
+verb = "asked";
+place = "forest";
+adjective2 = "small";
+noun2 = "apples";
+const secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory); 
