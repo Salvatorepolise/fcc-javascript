dea9845dd89dfe605a9cf942c01e906629b2af39
Salvatorepolise <salvatorepolisee@gmail.com>
2026-06-17 16:24:15 +0200

    debugged this file

---
+++ 2BooleansandNumbers/DebugTypeCoercionError.js
@@ -0,0 +1,17 @@
+const firstResult = 5 + 10;
+console.log(`5 + 10 = ${firstResult}`);
+
+const secondResult = 8 - 5;
+console.log(`8 - 5 = ${secondResult}`);
+
+const thirdResult = 5 + 1;
+console.log(`1 + 5 = ${thirdResult}`);
+
+const fourthResult = 7 + 1;
+console.log(`0 + 8 = ${fourthResult}`);
+
+const fifthResult = 10 * 2;
+console.log(`10 * 2 = ${fifthResult}`);
+
+const sixthResult = 22;
+console.log(`0 + 22 = ${sixthResult}`);
