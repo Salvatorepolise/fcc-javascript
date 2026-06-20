87ce04b9c7e6c7273e81f923698ccf41184412b7
Salvatorepolise <salvatorepolisee@gmail.com>
2026-06-20 17:48:10 +0200

    Created an email masker

---
+++ 3Functions/emailmasker.js
@@ -0,0 +1,18 @@
+let email = "apple.pie@example.com";
+
+function maskEmail(email) {
+
+  const atIndex = email.indexOf("@");
+  
+  const username = email.slice(0, atIndex);
+  const domain = email.slice(atIndex);
+
+  const maskedUsername = 
+    username[0] + 
+    "*".repeat(username.length - 2) + 
+    username[username.length - 1];
+  
+  return maskedUsername + domain;
+}
+
+console.log(maskEmail(email));
