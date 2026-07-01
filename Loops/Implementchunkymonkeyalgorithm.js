5f10833ecca45d200736fe11bb915bad5c8da44a
Salvatorepolise <salvatorepolisee@gmail.com>
2026-07-01 15:35:32 +0200

    Created Implement Chunky monkey algorithm

---
+++ Loops/Implementchunkymonkeyalgorithm.js
@@ -0,0 +1,9 @@
+function chunkArrayInGroups(arr, size) {
+  const chunks = [];
+  
+  for (let i = 0; i < arr.length; i += size) {
+    chunks.push(arr.slice(i, i + size));
+  }
+  
+  return chunks;
+}
