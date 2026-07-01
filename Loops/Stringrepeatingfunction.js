0964e53df0ccffc714cc20afd294c4861e952cc4
Salvatorepolise <salvatorepolisee@gmail.com>
2026-07-01 15:43:37 +0200

    Created a String repeating function

---
+++ Loops/Stringrepeatingfunction.js
@@ -0,0 +1,15 @@
+function repeatStringNumTimes(str, num) {
+  
+  if (num <= 0) {
+    return "";
+  }
+
+  let repeatedString = "";
+  
+ 
+  for (let i = 0; i < num; i++) {
+    repeatedString += str;
+  }
+  
+  return repeatedString;
+}
