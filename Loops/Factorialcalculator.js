e625b5ba6c111ba3c0e1784b607c805c330e6d6a
Salvatorepolise <salvatorepolisee@gmail.com>
2026-07-01 15:30:04 +0200

    Created a Factorial calculator

---
+++ Loops/Factorialcalculator.js
@@ -0,0 +1,16 @@
+const num = 7;
+
+function factorialCalculator(n) {
+  let result = 1;
+  
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  
+  return result;
+}
+const factorial = factorialCalculator(num);
+
+const resultMsg = `Factorial of ${num} is ${factorial}`;
+
+console.log(resultMsg);
