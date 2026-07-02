b3d464c038ca4a25a16749513c1ad0328a37f8d7
Salvatorepolise <salvatorepolisee@gmail.com>
2026-07-02 11:27:10 +0200

    Created an Inventory management program

---
+++ JavascriptFundamentalsReview/Inventorymanagementprogram.js
@@ -0,0 +1,40 @@
+let inventory = [];
+
+function findProductIndex(productName) {
+  const searchName = productName.toLowerCase();
+  return inventory.findIndex(item => item.name === searchName);
+}
+function addProduct(product) {
+  const name = product.name.toLowerCase();
+  const index = findProductIndex(name);
+
+  if (index !== -1) {
+    inventory[index].quantity += product.quantity;
+    console.log(name + " quantity updated");
+  } else {
+    inventory.push({ name: name, quantity: product.quantity });
+    console.log(name + " added to inventory");
+  }
+}
+function removeProduct(productName, quantity) {
+  const name = productName.toLowerCase();
+  const index = findProductIndex(name);
+
+  if (index === -1) {
+    console.log(name + " not found");
+    return;
+  }
+
+  const product = inventory[index];
+
+  if (product.quantity < quantity) {
+    console.log("Not enough " + name + " available, remaining pieces: " + product.quantity);
+  } else {
+    product.quantity -= quantity;
+    if (product.quantity === 0) {
+      inventory.splice(index, 1);
+    } else {
+      console.log("Remaining " + name + " pieces: " + product.quantity);
+    }
+  }
+}
